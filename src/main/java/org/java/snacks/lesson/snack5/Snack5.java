eab6c24be8b3378b8e959afbb36aee156b05157a
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-06-29 10:28:41 +0200

    add: snack 5

---
+++ src/main/java/org/java/snacks/lesson/snack5/Snack5.java
@@ -0,0 +1,33 @@
+package org.java.snacks.lesson.snack5;
+
+import java.util.HashMap;
+
+public class Snack5 {
+	
+public static void main(String[] args) {
+		
+	
+	HashMap<String,String> newMap = new HashMap<String,String>();
+	
+	newMap.put("Studente", "Fred");
+	
+	newMap.put("Età", "20");
+	
+	newMap.put("Classe", "Bept2");
+	
+	newMap.put("Studente", "Giacomo");
+		
+		
+	}
+
+
+public boolean checkKeys(HashMap checkMap) {
+	
+	
+	
+	
+	return false;
+
+}
+
+}
